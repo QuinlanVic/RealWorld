989978874ecc5a416b96765bed812704c1c75cec
Quinlan <Quinlanvc@gmail.com>
2023-04-13 15:47:57 +0200

    post fixed  (triple quotes) but still not done

--- src/Post.elm
+++ src/Post.elm
@@ -2,7 +2,7 @@ module Post exposing (..)
 
 import Html exposing (..)
 
-import Html.Attributes exposing (class, href, id, placeholder, rows, src, type_)
+import Html.Attributes exposing (class, href, id, placeholder, rows, src, target, type_)
 
 main : Html msg
 main =
@@ -45,30 +45,30 @@ main =
         , div [class "container-page"] 
             [div [class "row post-content"] 
                 [div [class "col-md-12"] 
-                    [ p [] [text "Web development technologies have evolved at an incredible clip over the past few years. 
+                    [ p [] [text """Web development technologies have evolved at an incredible clip over the past few years.
                             We&#39;ve gone from rudimentary DOM manipulation with libraries like jQuery to supercharged web 
                             applications organized &amp; powered by elegant MV* based frameworks like AngularJS. 
                             Pair this with significant increases in browser rendering speeds, and it is now easier than ever 
-                            before to build production quality applications on top of Javascript, HTML5, and CSS3."]
-                    , p [] [text "While these advances have been incredible, they are only just starting to affect the clear 
+                            before to build production quality applications on top of Javascript, HTML5, and CSS3."""]
+                    , p [] [text """While these advances have been incredible, they are only just starting to affect the clear 
                             platform of the future: mobile. For years, mobile rendering speeds were atrocious, and the MVC frameworks 
                             &amp; UI libraries provided by iOS and Android were far superior to writing mobile apps using web technologies. 
                             There were also some very public failures -- Facebook famously wrote their first iOS app in 2011 using HTML5 but 
-                            ended up scrapping it due to terrible performance."]
-                    , p [] [text "For years now, hybrid apps have been mocked and jeered by 
+                            ended up scrapping it due to terrible performance."""]
+                    , p [] [text """For years now, hybrid apps have been mocked and jeered by 
                             native app developers for being clunky and ugly, having subpar performance, and having no advantages over native apps. 
                             While these may have been valid reasons in 2011, they are now virtually baseless, thanks to a collection of new technologies
                             that have emerged over the past two years. With these technologies, you can design, build, and deploy robust mobile apps faster
                             than you could with native technologies, all while incurring little to no app performance penalties. This is thanks in large part
                             to super fast mobile browser rendering speeds and better JavaScript performance. This course is designed to teach you how to effectively
-                            use these new technologies to build insanely great mobile apps."]
-                    , p[] [text "Without further ado, we&#39;d like to welcome you to the future of
+                            use these new technologies to build insanely great mobile apps."""]
+                    , p[] [text """Without further ado, we&#39;d like to welcome you to the future of
                            mobile app development, freed from the shackles of native languages &amp; frameworks. 
-                           Let&#39;s learn what the new mobile stack consists of and how it works."] 
+                           Let&#39;s learn what the new mobile stack consists of and how it works."""] 
                     , h2 [id "introducing-ionic"] [text "Introducing ionic."]
-                    , p [] [ text "Before, building hybrid apps was a chore -- not because it was hard to build web pages, but because it was hard to build full-fledged web applications. 
+                    , p [] [ text """Before, building hybrid apps was a chore -- not because it was hard to build web pages, but because it was hard to build full-fledged web applications. 
                                     With AngularJS, that has changed. As a result, Angular became the core innovation that made hybrid apps possible. The bright folks at Drifty were some of the 
-                                    first to realize this and subsequently created the "
+                                    first to realize this and subsequently created the """
                            , a [href "http://ionicframework.com/", target "_blank"] [text "Ionic Framework"]
                            , text "to bridge the gap between AngularJS web apps and hybrid mobile apps. Since launching a little over a year ago, the Ionic Framework has "
                            , a [href "http://www.google.com/trends/explore?hl=en-US&amp;q=ionic+framework&amp;cmpt=q&amp;tz&amp;tz&amp;content=1", target "_blank"] [text "quickly grown in popularity amongst developers"]
@@ -79,8 +79,8 @@ main =
                            , a [href "https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIKit_Framework/", target"_blank"] [text "iOS UIKit"]
                            , text " provides for Obj-C/Swift, and that "
                            , a [href "http://developer.android.com/guide/topics/ui/overview.html", target "_blank"] [text "Android UI elements"]
-                           , text " provides for Java. Core mobile UI paradigms are available to developers out of the box, which means that developers can focus on building apps, 
-                                instead of common user interface elements. Some examples of these include "
+                           , text """ provides for Java. Core mobile UI paradigms are available to developers out of the box, which means that developers can focus on building apps, 
+                                instead of common user interface elements. Some examples of these include """
                            , a [href "http://ionicframework.com/docs/api/directive/ionList/", target "_blank"] [text "list views"]
                            , text ", " 
                            , a [href "http://ionicframework.com/docs/api/directive/ionNavView/", target "_blank"] [text "stateful navigation"]
@@ -91,49 +91,52 @@ main =
                            , text ", and "
                            , a [href "http://ionicframework.com/docs/nightly/api/", target "_blank"] [text "so much more"]
                            , text "."]
-                    , p [] [text "Ionic is a great solution for creating both mobile web apps and native apps. The first sections of this course will go over structuring Ionic apps that can run on the web. 
+                    , p [] [text """Ionic is a great solution for creating both mobile web apps and native apps. The first sections of this course will go over structuring Ionic apps that can run on the web. 
                             Then we will cover packaging that same exact code into a native app. We will be using a build tool called Cordova for packaging our app. For those unfamiliar with Cordova, it is
                             the open source core of Adobe&#39;s proprietary PhoneGap build system. Adobe describes it with this analogy: Cordova is to PhoneGap as Blink is to Chrome. Basically, PhoneGap is
-                            Cordova plus a whole bunch of other Adobe stuff."]
-                    , p [] [text "The folks at Ionic have done a fantastic job of making Cordova super easy to use by directly wrapping it in their &#39;ionic&#39; command line tool (don&#39;t worry, we&#39;ll cover this later).
-                            Just remember that Cordova is something that is running under the hood of your hybrid app that you will rarely need to worry about, but we will cover some common interactions with it in this course."]
+                            Cordova plus a whole bunch of other Adobe stuff."""]
+                    , p [] [text """The folks at Ionic have done a fantastic job of making Cordova super easy to use by directly wrapping it in their &#39;ionic&#39; command line tool (don&#39;t worry, we&#39;ll cover this later).
+                            Just remember that Cordova is something that is running under the hood of your hybrid app that you will rarely need to worry about, but we will cover some common interactions with it in this course."""]
                     , h2 [id "what-we-re-going-to-build"] [text "What we&#39;re going to build"]
-                    , p [] [text "We will be building an app called Songhop, a &quot;Tinder for music&quot; app that allows you to listen to 30-second song samples and favorite the ones you like. This is based on a real 
-                            Ionic/Cordova powered app we built that exists on the"
+                    , p [] [text """We will be building an app called Songhop, a &quot;Tinder for music&quot; app that allows you to listen to 30-second song samples and favorite the ones you like. This is based on a real 
+                            Ionic/Cordova powered app we built that exists on the """
                            , a [href "https://itunes.apple.com/us/app/songhop/id899245239?mt=8", target "_blank"] [text "iOS App Store"]
-                           , text "-- feel free to download it to get a feeling for what Ionic is capable of (and rate it 5 stars :). It&#39;s also worth noting that it only took us a month to build the Songhop app that&#39;s
-                             on the App Store, so that should give you an idea of how fast you can build &amp; iterate using Ionic / Cordova."]
+                           , text """ -- feel free to download it to get a feeling for what Ionic is capable of (and rate it 5 stars :). It&#39;s also worth noting that it only took us a month to build the Songhop app that&#39;s
+                             on the App Store, so that should give you an idea of how fast you can build &amp; iterate using Ionic / Cordova."""]
                     , p [] [strong [] [text "You can also see a "
                            , a [href "https://ionic-songhop.herokuapp.com", target "_blank"] [text "live demo of the completed application we&#39;ll be building here"]
                            , text " (resize your browser window to the size of a phone for the best experience)."]
                             ]
-                    ,p [] [ text "We&#39;ll be covering a wide variety of topics in this course: scaffolding a new application, testing it in the emulator, installing native plugins for manipulating audio &amp; 
-                            files, swipe gestures for our interface, installing the app on your own device, deploying to the iOS &amp; Android app stores, and so much more."]
+                    ,p [] [ text """We&#39;ll be covering a wide variety of topics in this course: scaffolding a new application, testing it in the emulator, installing native plugins for manipulating audio &amp; 
+                            files, swipe gestures for our interface, installing the app on your own device, deploying to the iOS &amp; Android app stores, and so much more."""]
                     ]
                 ]
+            ] 
             , hr [] [] 
             , div [class "post-actions"]
                 [div [class "post-meta"] 
                     [ a [href "profile.html"] [img [src "http://i.imgur.com/Qr71crq.jpg"] []]
                     , div [class "info"]
                         [ a [href "profile.html", class "author"] [text "Eric Simons"]
-                        , span [class "date"] [Text "January 20th"]
+                        , span [class "date"] [text "January 20th"]
                         ]
                     , button [class "btn btn-sm btn-outline-secondary"]
                         [ i [class "ion-heart"] []
                         , text "&nbsp; Favorite Post" --&nbsp;?
                         , span [class "counter"] [text "(29)"]
                         ]
+                    ]
                 ]
             ]
-        ]
     , footer []
         [ div [class "container"]
-            [ a [href "/", class "logo-font"] [text "conduit"]]
+            [ a [href "/", class "logo-font"] [text "conduit"]
             , span [class "attribution"] 
                 [ text "An interactive learning project from"
                 , a [href "https:..thinkster.io"] [text "Thinkster"]
                 , text "Code & design licensed under MIT."
-                ]
+                ] 
+            ]
         ]
     ]
+    
