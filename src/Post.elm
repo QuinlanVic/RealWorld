f892a8eed47750685de6620afc95d0f9b3bf52ad
QuinlanVic <Quinlanvc@gmail.com>
2023-04-14 11:04:02 +0200

    loadshedding

--- src/Post.elm
+++ src/Post.elm
@@ -184,7 +184,7 @@ main =
         [ div [class "container"]
             [ a [href "/", class "logo-font"] [text "conduit"]
             , span [class "attribution"] 
-                [ text "                                                An interactive learning project from "
+                [ text " An interactive learning project from "
                 , a [href "https:..thinkster.io"] [text "Thinkster"]
                 , text ". Code & design licensed under MIT."
                 ] 
