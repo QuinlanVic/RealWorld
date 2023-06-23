7ee9f8910878dba887e138200cf9a373d3c82668
QuinlanVic <Quinlanvc@gmail.com>
2023-06-23 17:22:05 +0200

    buttons

--- src/Login.elm
+++ src/Login.elm
@@ -1,27 +1,23 @@
 module Login exposing (main)
 
-import Html exposing (..)
-
-import Html.Attributes exposing (class, href, placeholder, style, type_)
-
-import Html.Events exposing (onClick, onInput)
 import Browser
+import Html exposing (..)
+import Html.Attributes exposing (class, href, placeholder, style, type_)
+import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode exposing (Decoder, bool, field, int, list, null, string, succeed)
+import Json.Decode.Pipeline exposing (hardcoded, required)
+import Json.Encode as Encode
 import Post exposing (Model)
 
-import Http
 
-import Json.Decode exposing (Decoder, bool, field, int, list, string, succeed) 
-
-import Json.Decode.Pipeline exposing (hardcoded, required)   
-
-import Json.Encode as Encode
-
-import Json.Decode exposing (null)
 
 --Model--
 -- type alias Model =
---     { user : Maybe User 
+--     { user : Maybe User
 --     }
+
+
 type alias User =
     { email : String --all of these fields are contained in the response from the server (besides last 3)
     , token : String
@@ -33,52 +29,66 @@ type alias User =
     , errmsg : String --display any API errors from authentication
     }
 
+
 baseUrl : String
-baseUrl = "http://127.0.0.1:8010/proxy/"    
+baseUrl =
+    "http://127.0.0.1:8010/proxy/"
+
 
 saveUser : User -> Cmd Msg
-saveUser user = 
+saveUser user =
     let
         body =
-            Http.jsonBody <| Encode.object [("user", (encodeUser <| user))]           
-    in 
+            Http.jsonBody <| Encode.object [ ( "user", encodeUser <| user ) ]
+    in
     Http.post
-        { body = body 
-        , expect = Http.expectJson LoadUser (userDecoder) -- wrap JSON received in LoadUser Msg    
+        { body = body
+        , expect = Http.expectJson LoadUser userDecoder -- wrap JSON received in LoadUser Msg
         , url = baseUrl ++ "api/users/login"
         }
 
+
 encodeUser : User -> Encode.Value
-encodeUser user = --used to encode user sent to the server via POST request body (for registering)
+encodeUser user =
+    --used to encode user sent to the server via POST request body (for registering)
     Encode.object
         [ ( "email", Encode.string user.email )
-        , ( "password", Encode.string user.password )   
+        , ( "password", Encode.string user.password )
         ]
 
+
+
 --userDecoder used for JSON decoding users returned when they register/sign-up
-userDecoder : Decoder User 
+
+
+userDecoder : Decoder User
 userDecoder =
     succeed User
         |> required "email" string
         |> required "token" string
         |> required "username" string
-        |> required "bio" string  
-        |> required "image" string    
+        |> required "bio" string
+        |> required "image" string
         |> hardcoded ""
-        |> hardcoded True     
-        |> hardcoded ""  
+        |> hardcoded True
+        |> hardcoded ""
         |> field "user"
+
+
 
 -- type alias Error =
 --     (FormField, String)
 
+
 getUserCompleted : User -> Result Http.Error User -> ( User, Cmd Msg )
 getUserCompleted user result =
-    case result of  
+    case result of
         Ok getUser ->
-            ({user | token = getUser.token, password = "", errmsg = ""} |> Debug.log "got the user", Cmd.none)  
+            ( { user | token = getUser.token, password = "", errmsg = "" } |> Debug.log "got the user", Cmd.none )
+
         Err error ->
-            ({user | errmsg = (Debug.toString error) }, Cmd.none)
+            ( { user | errmsg = Debug.toString error }, Cmd.none )
+
 
 initialModel : User
 initialModel =
@@ -87,119 +97,149 @@ initialModel =
     , username = ""
     , bio = ""
     , image = ""
-    , password = "" 
-    , loggedIn = False 
-    , errmsg = ""  
+    , password = ""
+    , loggedIn = False
+    , errmsg = ""
     }
 
-init : () -> (User, Cmd Msg)
+
+init : () -> ( User, Cmd Msg )
 init () =
-    (initialModel, Cmd.none)
+    ( initialModel, Cmd.none )
+
 
 fetchUser : Cmd Msg
 fetchUser =
-    Http.get 
+    Http.get
         { url = baseUrl ++ "api/users"
-        , expect = Http.expectJson LoadUser userDecoder 
+        , expect = Http.expectJson LoadUser userDecoder
         }
 
+
+
 --Update--
-update : Msg -> User -> (User, Cmd Msg)
-update message user = --what to do (update) with each message type
+
+
+update : Msg -> User -> ( User, Cmd Msg )
+update message user =
+    --what to do (update) with each message type
     case message of
-        SaveEmail email -> ({ user | email = email }, Cmd.none)
-        SavePassword password -> ({user | password = password }, Cmd.none)
-        Login -> ({ user | loggedIn = True }, saveUser user) 
+        SaveEmail email ->
+            ( { user | email = email }, Cmd.none )
+
+        SavePassword password ->
+            ( { user | password = password }, Cmd.none )
+
+        Login ->
+            ( { user | loggedIn = True }, saveUser user )
+
         -- LoadUser (Ok getUser) -> --confused here (return new model from the server with hardcoded password, errmsg and signedup values as those are not a part of the user record returned from the server?)
-        --     -- ({getUser | signedUp = True, password = "", errmsg = ""}, Cmd.none) 
-        --     -- ({getUser | signedUp = True, password = "", errmsg = ""} |> Debug.log "got the user", Cmd.none) 
-        --     ({user | email = getUser.email, token = getUser.token, username = getUser.username, bio = getUser.bio, image = getUser.image, password = "", errmsg = ""} |> Debug.log "got the user", Cmd.none)  
+        --     -- ({getUser | signedUp = True, password = "", errmsg = ""}, Cmd.none)
+        --     -- ({getUser | signedUp = True, password = "", errmsg = ""} |> Debug.log "got the user", Cmd.none)
+        --     ({user | email = getUser.email, token = getUser.token, username = getUser.username, bio = getUser.bio, image = getUser.image, password = "", errmsg = ""} |> Debug.log "got the user", Cmd.none)
         -- LoadUser (Err error) ->
         --     (user, Cmd.none)
-        LoadUser result -> getUserCompleted user result 
-        -- Error errormsg -> user 
+        LoadUser result ->
+            getUserCompleted user result
+
+
+
+-- Error errormsg -> user
+
 
 subscriptions : User -> Sub Msg
 subscriptions user =
     Sub.none
-        
+
+
+
 --View--
 -- getType : String -> String -> Msg
 -- getType messageType = --get the type of message that should be sent to update from the placeholder (name/email/pswd)
---     case messageType of  
---         "Email" -> SaveEmail  
+--     case messageType of
+--         "Email" -> SaveEmail
 --         "Password" -> SavePassword
---         _ -> Error    
-
+--         _ -> Error
 -- viewForm : String -> String -> Html Msg
 -- viewForm textType textHolder =
---     fieldset [class "form-group"] 
+--     fieldset [class "form-group"]
 --         [input [class "form-control form-control-lg", type_ textType, placeholder textHolder, onInput (getType textType)] []
 --         ]
-    
+
+
 view : User -> Html Msg
 view user =
-    div[]
-    [ nav[class "navbar navbar-light"]
-        [div [class "container"] 
-            [ a [class "navbar-brand", href "indexelm.html"] [text "conduit"],
-            ul [class "nav navbar-nav pull-xs-right"] --could make a function for doing all of this
-                [ li [class "nav-item"] [a [class "nav-link", href "indexelm.html"] [text "Home :)"]]
-                , li [class "nav-item"] [a [class "nav-link", href "editorelm.html"] [i [class "ion-compose"][], text (" " ++ "New Post")]]
-                , li [class "nav-item active"] [a [class "nav-link", href "loginelm.html"] [text "Log in"]]
-                , li [class "nav-item"] [a [class "nav-link", href "authelm.html"] [text "Sign up"]]
-                , li [class "nav-item"] [a [class "nav-link", href "settingselm.html"] [text "Settings"]]
-                ]
-            ]
-        ]
-    , div[class "auth-page"]
-        [ div[class "container page"]
-            [div [class "row"]
-                [div[class "col-md-6 col-md-offset-3 col-xs-12"]
-                    [h1 [class "text-xs-center"] [text "Log in"],
-                    p [class "text-xs-center"] [a [href "authelm.html"] [text "Don't have an account?"]]   
-                    -- , div [ class showError ]
-                    --     [ div [ class "alert alert-danger" ] [ text user.errmsg ]
-                    --     ]       
-                    , form []
-                    -- [ viewForm "text" "Your Name"
-                    -- , viewForm "text" "Email"
-                    -- , viewForm "password" "Password"
-                    [ fieldset [class "form-group"] [input [class "form-control form-control-lg", type_ "email", placeholder "Email", onInput SaveEmail] []]
-                    , fieldset [class "form-group"] [input [class "form-control form-control-lg", type_ "password", placeholder "Password", onInput SavePassword] []]
-                    , button [class "btn btn-lg btn-primary pull-xs-right", onClick Login] [text "Log In"]
-                    ]
+    div []
+        [ nav [ class "navbar navbar-light" ]
+            [ div [ class "container" ]
+                [ a [ class "navbar-brand", href "indexelm.html" ] [ text "conduit" ]
+                , ul [ class "nav navbar-nav pull-xs-right" ]
+                    --could make a function for doing all of this
+                    [ li [ class "nav-item" ] [ a [ class "nav-link", href "indexelm.html" ] [ text "Home :)" ] ]
+                    , li [ class "nav-item" ] [ a [ class "nav-link", href "editorelm.html" ] [ i [ class "ion-compose" ] [], text (" " ++ "New Post") ] ]
+                    , li [ class "nav-item active" ] [ a [ class "nav-link", href "loginelm.html" ] [ text "Log in" ] ]
+                    , li [ class "nav-item" ] [ a [ class "nav-link", href "authelm.html" ] [ text "Sign up" ] ]
+                    , li [ class "nav-item" ] [ a [ class "nav-link", href "settingselm.html" ] [ text "Settings" ] ]
                     ]
                 ]
             ]
-        ]
-    , footer []
-        [ div [class "container"]
-            [ a [href "/", class "logo-font"] [text "conduit"]
-            , text " " --helps make spacing perfect even though it's not exactly included in the og html version
-            , span [class "attribution"] 
-                [ text "An interactive learning project from "
-                , a [href "https:..thinkster.io"] [text "Thinkster"]
-                , text ". Code & design licensed under MIT."
-                ] 
-            ]
-        ]
-    ]
---div is a function that takes in two arguments, a list of HTML attributes and a list of HTML children
+        , div [ class "auth-page" ]
+            [ div [ class "container page" ]
+                [ div [ class "row" ]
+                    [ div [ class "col-md-6 col-md-offset-3 col-xs-12" ]
+                        [ h1 [ class "text-xs-center" ] [ text "Log in" ]
+                        , p [ class "text-xs-center" ] [ a [ href "authelm.html" ] [ text "Don't have an account?" ] ]
 
+                        -- , div [ class showError ]
+                        --     [ div [ class "alert alert-danger" ] [ text user.errmsg ]
+                        --     ]
+                        , form []
+                            -- [ viewForm "text" "Your Name"
+                            -- , viewForm "text" "Email"
+                            -- , viewForm "password" "Password"
+                            [ fieldset [ class "form-group" ] [ input [ class "form-control form-control-lg", type_ "email", placeholder "Email", onInput SaveEmail ] [] ]
+                            , fieldset [ class "form-group" ] [ input [ class "form-control form-control-lg", type_ "password", placeholder "Password", onInput SavePassword ] [] ]
+                            , button [ class "btn btn-lg btn-primary pull-xs-right", type_ "button", onClick Login ] [ text "Log In" ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        , footer []
+            [ div [ class "container" ]
+                [ a [ href "/", class "logo-font" ] [ text "conduit" ]
+                , text " " --helps make spacing perfect even though it's not exactly included in the og html version
+                , span [ class "attribution" ]
+                    [ text "An interactive learning project from "
+                    , a [ href "https:..thinkster.io" ] [ text "Thinkster" ]
+                    , text ". Code & design licensed under MIT."
+                    ]
+                ]
+            ]
+        ]
+
+
+
+--div is a function that takes in two arguments, a list of HTML attributes and a list of HTML children
 --messages for defining what the update is to do upon interactivity
-type Msg 
+
+
+type Msg
     = SaveEmail String
     | SavePassword String
-    | Login 
-    | LoadUser (Result Http.Error User) 
-    -- | Error String 
+    | Login
+    | LoadUser (Result Http.Error User)
 
-main : Program () User Msg 
+
+
+-- | Error String
+
+
+main : Program () User Msg
 main =
-    Browser.element 
+    Browser.element
         { init = init
         , view = view
-        , update = update 
-        , subscriptions = subscriptions 
+        , update = update
+        , subscriptions = subscriptions
         }
